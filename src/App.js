cb6ba5713a6eba4a9eaf2e17ebb06d87ae472f5a
Daniel Park <abideinhope@gmail.com>
2018-06-16 11:51:57 +0900

    Passing switchNameHandler to a Child through props

--- src/App.js
+++ src/App.js
@@ -28,8 +28,13 @@ class App extends Component {
         <Container className="main">
           <h1>Hi, I'm a React App</h1>
           <Button onClick={this.switchNameHandler} content='Switch Name'></Button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>This is Children of Person</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>This is Children of Person</Person>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+          <Person 
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler}>This is Children of Person
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
         </Container>
       </div>
     );
