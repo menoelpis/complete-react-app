8c6cc75a0cc2f083703dcca4ebc1df99b79383ab
Daniel Park <abideinhope@gmail.com>
2018-06-16 11:33:58 +0900

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import { Button, Container } from 'semantic-ui-react';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container className="main">
+          <h1>Hi, I'm a React App</h1>
+          <Button onClick={this.switchNameHandler} content='Switch Name'></Button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>This is Children of Person</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>This is Children of Person</Person>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
